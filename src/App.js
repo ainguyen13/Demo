92d0aad7b43c2159b8e354441f6679d6ee998482
ainguyen13 <vietai1310@gmail.com>
2022-11-15 02:29:05 +0700

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import CreateAuctions from './mainFunctions/CreateAuction';
+import MyBids from './mainFunctions/MyBids';
+import MyAuctions from './mainFunctions/MyAuctions';
+
 
 function App() {
-  return (
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateAuctions></CreateAuctions>
+      <MyBids></MyBids>
+      <MyAuctions></MyAuctions>
     </div>
   );
 }
+
 
 export default App;
